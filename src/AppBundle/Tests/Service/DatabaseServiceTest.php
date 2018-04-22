485daa0200137514c7a9b74d71658a6ccedc1713
= <=>
2018-04-22 12:39:13 -0300

    Add Test Database Service
    Implements tests on methods class database service

---
+++ src/AppBundle/Tests/Service/DatabaseServiceTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DatabaseServiceTest extends WebTestCase
+{
+
+    protected $serviceDatabase;
+
+    public function setUp()
+    {
+        $this->serviceDatabase = new \AppBundle\Service\DatabaseService(
+            '127.0.0.1',
+            '27017',
+            'easytaxi-symfony-cache-test'
+        );
+    }
+
+    public function testGetDatabase()
+    {
+        $this->assertEquals('easytaxi-symfony-cache-test', $this->serviceDatabase->getDatabase());
+    }
+
+    public function testExecuteQueryReturnEmpty()
+    {
+        $customers = $this->serviceDatabase->executeQuery('customers');
+        $customers = iterator_to_array($customers);
+
+        $this->assertEmpty($customers);
+    }
+
+    public function testBulkWrite()
+    {
+        $data = [
+            [
+                'name' => 'Valdomiro Santiago',
+                'age' => 57
+            ],
+            [
+                'name' => 'Edir Macedo',
+                'age' => 89
+            ]
+        ];
+
+        $bulkWrite = $this->serviceDatabase->bulkWrite('customers', $data);
+
+        $this->assertEquals(2, $bulkWrite->getInsertedCount());
+    }
+
+    public function testExecuteQueryReturnNotEmpty()
+    {
+        $customers = $this->serviceDatabase->executeQuery('customers');
+        $customers = iterator_to_array($customers);
+
+        $this->assertNotEmpty($customers);   
+    }
+
+    public function testDropCollection()
+    {
+        $this->serviceDatabase->drop('customers');
+
+        $customers = $this->serviceDatabase->executeQuery('customers');
+        $customers = iterator_to_array($customers);
+
+        $this->assertEmpty($customers);
+    }
+
+}
